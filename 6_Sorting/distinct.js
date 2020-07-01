1dd04d1cb2f0a8fd8daf31c0081849a6bba5c341
Max Hardy <max@hardy.work>
2020-07-01 09:48:42 +0100

    Convert indentation to spaces

--- 6_Sorting/distinct.js
+++ 6_Sorting/distinct.js
@@ -4,14 +4,14 @@
  * @param {number[]} A is an array (where each element is an integer within the range [−1,000,000..1,000,000])
  */
 function solution(A) {
-	const N = A.length;
-	const counters = {};
+    const N = A.length;
+    const counters = {};
 
-	for (const currentValue of A) {
-		counters[currentValue] = 1;
-	}
+    for (const currentValue of A) {
+        counters[currentValue] = 1;
+    }
 
-	return Object.keys(counters).length;
+    return Object.keys(counters).length;
 }
 
 module.exports = solution;
