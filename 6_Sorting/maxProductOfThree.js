a2f66e6414a8cebfbc77ff7dd9198e38385af412
Max Hardy <max@hardy.work>
2020-07-01 09:47:59 +0100

    Convert indentation to spaces

--- 6_Sorting/maxProductOfThree.js
+++ 6_Sorting/maxProductOfThree.js
@@ -4,11 +4,10 @@
  * @param {number[]} A is an array (where each element is an integer within the range [−1,000..1,000])
  */
 function solution(A) {
-	const N = A.length;
-	
-	A.sort((a, b) => a - b);
+    const N = A.length;
+    A.sort((a, b) => a - b);
 
-	return Math.max((A[N - 1] * A[N - 2] * A[N - 3]), (A[0] * A[1] * A[N - 1]));
+    return Math.max((A[N - 1] * A[N - 2] * A[N - 3]), (A[0] * A[1] * A[N - 1]));
 }
 
 module.exports = solution;
